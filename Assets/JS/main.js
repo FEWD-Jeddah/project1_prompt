e5f3888a31bc123bef87fa92542c554c2aed46ac
Mullham Sahhaf <mullham.sahhaf@fmcg.mynaghi.com>
2019-12-17 20:07:52 +0300

    Update main.js

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -90,7 +90,7 @@ $(function(){
 
     $("#choco_quiz").on("click",function(){
         Swal.fire({
-            title:'Leaving so Soon?',
+            title:'Leaving so Soon -_-',
             text:'You are about to leave this webpage, do you want to continue?',
             icon:'question',
             showCancelButton: true
